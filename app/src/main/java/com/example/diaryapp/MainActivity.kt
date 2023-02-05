982341c8839e3646dd5d4953b5c591b78e52d241
Abhijeet <demoionic2021@gmail.com>
2023-02-05 12:35:01 +0530

    completed Navigation setup

--- app/src/main/java/com/example/diaryapp/MainActivity.kt
+++ app/src/main/java/com/example/diaryapp/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.example.diaryapp.navigation.Screen
+import com.example.diaryapp.navigation.SetupNavGraph
 import com.example.diaryapp.ui.theme.DiaryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,13 +22,11 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             DiaryAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                val navController = rememberNavController()
+                SetupNavGraph(
+                    startDestination = Screen.Authentication.route,
+                    navController = navController
+                )
             }
         }
     }
--- app/src/main/java/com/example/diaryapp/util/Constants.kt
+++ app/src/main/java/com/example/diaryapp/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
 
     const val APP_ID = "mydiaryapp-oxizx"
     const val CLIENT_ID = "604141205154-hp9ri5ja2b584mcc7einosv1a9to7f74.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
 }
